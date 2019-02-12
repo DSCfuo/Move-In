e564dad3f5887c76197b29bd13cdab3c51df2add
georgeben <msdcconnect@gmail.com>
2019-02-12 16:03:32 +0100

    Created searchbox component

---
+++ client/src/components/SearchBox.vue
@@ -0,0 +1,87 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-text>
+                <v-form v-model="valid" ref="form">
+                    <v-container>
+                    <v-layout>
+                        <v-flex
+                        xs12
+                        md4
+                        >
+                        <v-text-field
+                            v-model="location"
+                            label="Enter location"
+                            clearable
+                            required
+                            prepend-icon="location_on"
+                            :rules="locationRules"
+                        ></v-text-field>
+                        </v-flex>
+
+                        <v-flex
+                        xs12
+                        md3
+                        >
+                        <v-text-field
+                            v-model="budget"
+                            label="Your Budget"
+                            required
+                            :rules="budgetRules"
+                            type="number"
+                            prefix="N"
+                        ></v-text-field>
+                        </v-flex>
+
+                        <v-flex
+                        xs12
+                        md3
+                        >
+                         <v-select
+                            :items="apartmentType"
+                            v-model="apartment"
+                            label="Apartment Type"
+                            prepend-icon="home"
+                            ></v-select>
+                        </v-flex>
+
+                        <v-flex
+                        xs12
+                        md2
+                        >
+                         <v-btn class="primary" @click="submit">Find apartment</v-btn>
+                        </v-flex>
+                    </v-layout>
+                    </v-container>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      location: '',
+      budget: undefined,
+      apartment:'',
+      apartmentType: ['Office', 'Shop', 'Self Contain',
+                      '1 Bedroom apartment', '2 Bedroom apartment',
+                      '3 Bedroom apartment', 'Duplex'],
+      budgetRules: [
+          value  => value >= 20000 || 'Budget cannot be less than 20,000 naira'
+      ],
+      locationRules: [
+          value => value.length >= 3 || 'Location must be at least three characters long'
+      ]
+    }),
+    methods: {
+        submit(){
+            if(this.$refs.form.validate()){
+                console.log(this.location, this.budget, this.apartment)
+            }
+        }
+    }
+  }
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="home">
     <div class="showcase pt-5">
-      <h2 class="display-1 mx-auto white--text">Find the perfect apartment</h2>
+      <h2 class="display-2 mx-auto white--text">Find the perfect apartment</h2>
       <h5 class="title mx-auto white--text">Lorem ipsum dolor sit amet, consectetur adipiscing do eiusmod tempo incididunt ut labore et dolore magna aliqua.</h5>
+
+      <SearchBox />
     </div>
   </div>
 </template>
 
 <script>
-
+import SearchBox from '@/components/SearchBox.vue'
   export default {
-    components: {}
+    components: {
+      SearchBox,
+    }
   }
 </script>
 
 <style scoped>
 .showcase{
   text-align: center;
-  height: 400px;
+  min-height: 400px;
   background: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0,0,0, 0.59)), url("../../public/images/apartment.jpg") center no-repeat ;
 }
 
