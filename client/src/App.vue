cc52300026905d8154806b822f82ee68de67af79
georgeben <msdcconnect@gmail.com>
2019-02-12 14:33:54 +0100

    Styled showcase section of the home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -22,3 +22,8 @@ export default {
   }
 }
 </script>
+
+<style>
+
+</style>
+
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="home">
+    <div class="showcase pt-5">
+      <h2 class="display-1 mx-auto white--text">Find the perfect apartment</h2>
+      <h5 class="title mx-auto white--text">Lorem ipsum dolor sit amet, consectetur adipiscing do eiusmod tempo incididunt ut labore et dolore magna aliqua.</h5>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +13,16 @@
     components: {}
   }
 </script>
+
+<style scoped>
+.showcase{
+  text-align: center;
+  height: 400px;
+  background: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0,0,0, 0.59)), url("../../public/images/apartment.jpg") center no-repeat ;
+}
+
+.showcase h2, .showcase h5{
+  width: 60%;
+  line-height: 1.5em !important;
+}
+</style>
