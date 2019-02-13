d9cb797ebe8e1815d9ad68b0c4d2566254a749a1
georgeben <msdcconnect@gmail.com>
2019-02-13 12:59:37 +0100

    Created server starter files

---
+++ api/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const port = 3000;
+const NODE_ENV = process.env.NODE_ENV;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the MoveIn API"
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Page not found'
+    })
+})
+
+app.use((err, req, res, next) => {
+    res.json({
+        message: err.message
+    })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
