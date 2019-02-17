ba5314c6671a9b3435273764bf3ec1a72cc2b7ff
georgeben <msdcconnect@gmail.com>
2019-02-17 22:57:40 +0100

    Fixed the useless evil :rage: search bug

--- api/routes/apartments.js
+++ api/routes/apartments.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const apartmentController = require('../controllers/apartments')
 
 router.get('/', apartmentController.getAllApartments);
-router.get('/:id', apartmentController.getApartmentById)
 router.get('/search', apartmentController.searchForApartment);
+router.get('/:id', apartmentController.getApartmentById)
 router.post('/', apartmentController.createApartment);
 router.put('/:id', apartmentController.updateApartment);
 router.delete('/:id', apartmentController.deleteApartment)
